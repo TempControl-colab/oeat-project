a1593bc56047ca58a2d2e93d7271f77cf7189794
Escias <ljimmy98@hotmail.fr>
2020-08-11 22:49:03 +0200

    Register Page, only visual

---
+++ flutter_app/lib/frontPage/FrontConnect.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutterapp/blockGestion/GestionSwitch.dart';
+import 'package:flutterapp/connecteur/UserService.dart';
+import 'package:flutterapp/elementsArgument/ElementsAppBox.dart';
+import 'package:flutterapp/style/StyleApp.dart';
+
+class FrontConnect {
+  ElementsAppBox _ELEMENTS = ElementsAppBox();
+
+  Widget FrontC (BuildContext context, State state) {
+    return centerScreen(context, state);
+  }
+
+  Widget centerScreen(BuildContext context, State state) {
+    return Center(
+
+      child: Stack(
+        children: [
+          Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height/1,
+              width: MediaQuery.of(context).size.width/1,
+              child: Opacity(
+                opacity: 1.0,
+                child: Card(
+                  color: Color.fromRGBO(255, 255, 255, 0.1),
+                ),
+              ),
+            ),
+          ),
+          Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width/1,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.lightBlue.withOpacity(0.6),
+                      spreadRadius: 5,
+                      blurRadius: 5,
+                      offset: Offset(0, 0), // changes position of shadow
+                    ),
+                  ],
+                  border: Border(
+                      bottom: BorderSide(color: Colors.white, width: 2.5)
+                  ),
+                ),
+                child: Text(
+                  _ELEMENTS.CONNEXION.toUpperCase(),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                  textAlign: TextAlign.center,
+                  textScaleFactor: 2.0,
+                ),
+              )
+
+          ),
+          Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width/1.5,
+                    child: _ELEMENTS.whiteTextField(_ELEMENTS.MAILADRESS),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width/1.5,
+                    child: _ELEMENTS.whiteTextField(_ELEMENTS.PASSWORD),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(left: 0, top: 200, right: 0, bottom: 0),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.lightBlue.withOpacity(0.6),
+                            spreadRadius: 1,
+                            blurRadius: 5,
+                            offset: Offset(0, 0), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child : RaisedButton(
+                          textColor: Colors.blue,
+                          color: Colors.black.withOpacity(0.4),
+                          onPressed: () {},
+                          shape: Border(
+                            top: BorderSide(
+                                color: Colors.white,
+                                width: 2.5,
+                                style: BorderStyle.solid
+                            ),
+                            bottom: BorderSide(
+                                color: Colors.white,
+                                width: 2.5,
+                                style: BorderStyle.solid
+                            ),
+                          ),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width/1,
+                            child: Text(_ELEMENTS.OKBUTTON, style: TextStyle(fontSize: 20, color: Colors.white), textAlign: TextAlign.center,),
+                          )
+                      )
+                  ),
+                ],
+              )
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+
+            child: Container(
+              child: Stack(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround, //spaceBetween pour le rendu final
+                    children: [
+                      FlatButton(
+                        color: Colors.transparent,
+                        textColor: Colors.lightBlue,
+                        disabledColor: Colors.grey,
+                        disabledTextColor: Colors.black,
+                        padding: EdgeInsets.all(0.0),
+                        splashColor: Colors.blueAccent,
+                        onPressed: () {},
+                        child: Text(
+                          _ELEMENTS.FORGOTPASSWORD,
+                          style: TextStyle(fontSize: 12.0),
+                        ),
+                      ),
+                      FlatButton(
+                        color: Colors.transparent,
+                        textColor: Colors.lightBlue,
+                        disabledColor: Colors.grey,
+                        disabledTextColor: Colors.black,
+                        padding: EdgeInsets.all(0.0),
+                        splashColor: Colors.blueAccent,
+                        onPressed: () {},
+                        child: Text(
+                          _ELEMENTS.REGISTER,
+                          style: TextStyle(fontSize: 12.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  bool test() {
+    UserService userService = new UserService();
+    userService.getAllUsers();
+    return true;
+  }
+
+}
--- flutter_app/lib/frontPage/FrontInscription.dart
+++ flutter_app/lib/frontPage/FrontInscription.dart
@@ -1,11 +1,226 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterapp/elementsArgument/ElementsAppBox.dart';
 
-class FrontInscription{
+class FrontInscription extends StatefulWidget {
+  FrontInscription({Key key}) : super(key: key);
 
+  @override
+  _FrontInscription createState() => _FrontInscription();
+}
 
-  Widget FrontI(BuildContext context){
-    return Center(child: Text('test'),);
+class _FrontInscription extends State<FrontInscription>{
+
+  ElementsAppBox _ELEMENTS = ElementsAppBox();
+  String _nom;
+  bool _checkValue = false;
+  bool _checkValue2 = false;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          autovalidate: _ELEMENTS.AUTOVALIDATE,
+          child: Stack(
+            children: [
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    Container(
+                      width: MediaQuery.of(context).size.width/1.2,
+                      child: TextField(
+                        style: TextStyle(color: Colors.white),
+                        onChanged: (String s) {
+                          _nom = s;
+                        },
+                        onSubmitted: (String s) {
+                          _nom = s;
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Nom',
+                          hintStyle: TextStyle(
+                            color: Colors.white,
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.white
+                            ),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.white
+                            ),
+                          ),
+                          prefixIcon: Icon(Icons.person_outline,
+                            color: Colors.white),
+                        ),
+                        keyboardType: TextInputType.name,
+
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width/1.2,
+                      child: _ELEMENTS.classicTextField(_ELEMENTS.MAIL, Colors.white, TextInputType.emailAddress, Icon(Icons.mail_outline, color: Colors.white)),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width/1.2,
+                      child: _ELEMENTS.classicTextField(_ELEMENTS.PASSWORD, Colors.white, TextInputType.visiblePassword, Icon(Icons.lock_outline, color: Colors.white)),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width/1.2,
+                      child: _ELEMENTS.classicTextField(_ELEMENTS.CPASSWORD, Colors.white, TextInputType.visiblePassword, Icon(Icons.lock_outline, color: Colors.white)),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width/1.2,
+                      child: _ELEMENTS.classicTextField(_ELEMENTS.ADDRESS, Colors.white, TextInputType.text, Icon(Icons.location_on_outlined, color: Colors.white)),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width/5,
+                          child: _ELEMENTS.classicTextField(_ELEMENTS.FLOOR, Colors.white, TextInputType.number, null),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/4,
+                          child: _ELEMENTS.classicTextField(_ELEMENTS.CODE, Colors.white, TextInputType.text, null),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width/2.9,
+                          child: _ELEMENTS.classicTextField(_ELEMENTS.ZIP, Colors.white, TextInputType.number, null),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/2.5,
+                          child: _ELEMENTS.classicTextField(_ELEMENTS.CITY, Colors.white, TextInputType.name, null),
+                        ),
+                      ],
+                    ),
+                    Container(
+                        width: MediaQuery.of(context).size.width/1.2,
+                        child: _ELEMENTS.classicTextField(_ELEMENTS.DOOR, Colors.white, TextInputType.text, null)
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width/2,
+                          child: Expanded(
+                            child: SwitchListTile(
+                              title: Text(_ELEMENTS.ELEVATOR,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              value: _checkValue,
+                              onChanged: (bool value) {
+                                setState(() {
+                                  _checkValue = value;
+                                });
+                              },
+                              activeColor: Colors.lightBlueAccent,
+                              inactiveThumbColor: Colors.grey,
+                              inactiveTrackColor: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width/2,
+                          child: Expanded(
+                            child: SwitchListTile(
+                              title: Text(_ELEMENTS.ANIMAL,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              value: _checkValue2,
+                              onChanged: (bool value) {
+                                setState(() {
+                                  _checkValue2 = value;
+                                });
+                              },
+                              activeColor: Colors.lightBlueAccent,
+                              inactiveThumbColor: Colors.grey,
+                              inactiveTrackColor: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    /*Container(
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.red,
+                              blurRadius: 7.0,
+                              spreadRadius: 3.0,
+                            ),
+                          ]
+                      ),
+                      child: RaisedButton(
+                        onPressed: _validateInputs,
+                          child: Text(_ELEMENTS.VALIDATE),
+                      ),
+                    ),*/
+                    Container(
+                        margin: EdgeInsets.only(left: 0, top: 10, right: 0, bottom: 0),
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.lightBlue.withOpacity(0.6),
+                              spreadRadius: 1,
+                              blurRadius: 5,
+                              offset: Offset(0, 0), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                        child : RaisedButton(
+                            textColor: Colors.blue,
+                            color: Colors.black.withOpacity(0.4),
+                            onPressed: () {},
+                            shape: Border(
+                              top: BorderSide(
+                                  color: Colors.white,
+                                  width: 2.5,
+                                  style: BorderStyle.solid
+                              ),
+                              bottom: BorderSide(
+                                  color: Colors.white,
+                                  width: 2.5,
+                                  style: BorderStyle.solid
+                              ),
+                            ),
+                            child: Container(
+                              width: MediaQuery.of(context).size.width/1,
+                              child: Text(_ELEMENTS.REGISTER.toUpperCase(), style: TextStyle(fontSize: 20, color: Colors.white), textAlign: TextAlign.center,),
+                            )
+                        )
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
 
+
+  void _validateInputs(){
+
+  }
 }
